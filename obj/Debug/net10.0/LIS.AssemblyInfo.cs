a98a6f28d2c0a012cc2c0968edbdce1146c2f348
IrhamZaf <85091440+IrhamZaf@users.noreply.github.com>
2026-05-06 00:26:17 +0800

    publish

--- obj/Debug/net10.0/LIS.AssemblyInfo.cs
+++ obj/Debug/net10.0/LIS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LIS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+166cdceb6ddb14df12a3fd7db5bcfdf38bcc6ac7")]
 [assembly: System.Reflection.AssemblyProductAttribute("LIS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LIS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
